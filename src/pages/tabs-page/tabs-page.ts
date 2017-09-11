c4c694ac79491cb2629b82b6fedd66ef6baf83c6
Brad Orego <me@bradorego.com>
2017-09-10 20:15:11 -0500

    get rid of schedule, speakers tabs

--- src/pages/tabs-page/tabs-page.ts
+++ src/pages/tabs-page/tabs-page.ts
@@ -4,18 +4,14 @@ import { NavParams } from 'ionic-angular';
 
 import { AboutPage } from '../about/about';
 import { MapPage } from '../map/map';
-import { SchedulePage } from '../schedule/schedule';
-import { SpeakerListPage } from '../speaker-list/speaker-list';
 
 @Component({
   templateUrl: 'tabs-page.html'
 })
 export class TabsPage {
   // set the root pages for each tab
-  tab1Root: any = SchedulePage;
-  tab2Root: any = SpeakerListPage;
-  tab3Root: any = MapPage;
-  tab4Root: any = AboutPage;
+  tab1Root: any = MapPage;
+  tab2Root: any = AboutPage;
   mySelectedIndex: number;
 
   constructor(navParams: NavParams) {
