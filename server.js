3e5053d80d9248bbb387340527e597740d5f4cf0
Brad Orego <me@bradorego.com>
2017-09-16 09:25:06 -0500

    more heroku garbage

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
-var express = require('express'),
-app = express();
+var express = require('express');
+var compression = require('compression');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var app = express();
+
 app.use(express.static('www'));
-app.set('port', process.env.PORT || 8010);
+app.use(compression());
+app.use(bodyParser.json());
+app.use(cors());
+app.use(logger('dev'));
+
+app.set('port', process.env.PORT || 8080);
+// Initialize the app.
 app.listen(app.get('port'), function () {
-    console.log('Express server listening on port ' + app.get('port'));
+  console.log("You're a wizard, Harry. I'm a what? Yes, a wizard, on port", app.get('port'));
 });
+
+// app.listen(app.get('port'), function () {
+//     console.log('Express server listening on port ' + app.get('port'));
+// });
