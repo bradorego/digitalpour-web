d240f2022601fd5aeb060bb1384986f4147600f2
Brad Orego <me@bradorego.com>
2017-09-16 09:35:11 -0500

    adding heartbeat for heroku

--- server.js
+++ server.js
@@ -5,18 +5,23 @@ var bodyParser = require('body-parser');
 var cors = require('cors');
 var app = express();
 
-app.use(express.static('www'));
 app.use(compression());
 app.use(bodyParser.json());
 app.use(cors());
 app.use(logger('dev'));
+app.use(express.static('www'));
 
 app.set('port', process.env.PORT || 8080);
 // Initialize the app.
+// app.listen(app.get('port'), function () {
+//   console.log("You're a wizard, Harry. I'm a what? Yes, a wizard, on port", app.get('port'));
+// });
+
 app.listen(app.get('port'), function () {
-  console.log("You're a wizard, Harry. I'm a what? Yes, a wizard, on port", app.get('port'));
+  console.log('Express server listening on port ' + app.get('port'));
 });
 
-// app.listen(app.get('port'), function () {
-//     console.log('Express server listening on port ' + app.get('port'));
-// });
+app.get("/heartbeat", (req, res, next) => {
+  res.send("hello");
+});
+
